ab02456d4b0f72984c26672ebb5533f029afe545
Amit Gaur <amitgaur@gmail.com>
2014-08-08 08:47:09 -0700

    refactor logger and add static file

--- app/routes.js
+++ app/routes.js
@@ -1,13 +1,8 @@
-
 module.exports = function (app) {
     
     app.use("/*", function(req, res,next) {
         res.setHeader('Last-Modified', (new Date()).toUTCString());
-        next();
-    });
-    app.use("/", function(req, res) {
         res.sendfile('./public/views/index.html');
-        
     });
     app.use("/index", function(req, res) {
         var obj = {"hi" : "bi"};
--- server.js
+++ server.js
@@ -4,27 +4,43 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var winston = require('winston');
-var   expressWinston = require('express-winston');
+var expressWinston = require('express-winston');
 
-//====End Imports======
+function logger() { 
+ return    expressWinston.logger({
+        transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        }),
+            new winston.transports.File({
+                filename : __dirname + '/log/app.log',
+            json: true,
+            colorize: true,
+            handleExceptions : true,
+
+            }),
+            ],
+           meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+           msg: "HTTP {{req.method}} {{req.url}}" // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    });
+
+}
+module.exports.logger = logger;
+    
+
+
+
+    //====End Imports======
 
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
 app.use(methodOverride('X-HTTP-Method-Override'));
-app.use(express.static(__dirname + '/public'));
 //winston logger
-app.use(expressWinston.logger({
-    transports: [
-    new winston.transports.Console({
-        json: true,
-        colorize: true
-    })
-],
-    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
-    msg: "HTTP {{req.method}} {{req.url}}" // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
-}));
+app.use(logger());
+app.use(express.static(__dirname+'/public'));
 require('./app/routes')(app);
 
 //ports/serverip
@@ -36,5 +52,5 @@ var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 app.listen(server_port);
 
 exports = module.exports = app;
-
+exports = module.exports = logger;
 
